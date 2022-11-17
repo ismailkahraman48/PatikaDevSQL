dca05616ba90292b905f55a4cd50139b0eac8df4
İsmail Kahraman <76534257+ismailkahraman48@users.noreply.github.com>
2022-11-17 21:09:51 +0300

    Add files via upload

---
+++ odev4.sql
@@ -0,0 +1,19 @@
+-- SELECT DISTINCT replacement_cost
+-- FROM film
+
+-- SELECT COUNT(DISTINCT replacement_cost)
+-- FROM film
+
+-- SELECT COUNT(*)
+-- FROM film
+-- WHERE title ILIKE 't%' AND rating = 'G'
+
+-- SELECT COUNT(*)
+-- FROM country
+-- WHERE country ~~ '_____'
+
+SELECT COUNT(*)
+FROM city
+WHERE city ILIKE '%r'
+
+
